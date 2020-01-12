0017a157de2dbd9db65d1c34bd62f7f531a0db75
Iwaide <iwaide@hoobby.net>
2020-01-12 15:19:52 +0900

    Add padding

--- lib/widgets/image_list.dart
+++ lib/widgets/image_list.dart
@@ -10,15 +10,28 @@ class ImageList extends StatelessWidget {
     return ListView.builder(
       itemCount: images.length,
       itemBuilder: (context, int index) {
-        return Container(
-          padding: EdgeInsets.all(20.0),
-          margin: EdgeInsets.all(20.0),
-          decoration: BoxDecoration(
-            border: Border.all(color: Colors.grey, )
-          ),
-          child: Image.network(images[index].url),
-        );
+        return buildImage(images[index]);
       },
+    );
+  }
+
+  Widget buildImage(ImageModel image) {
+    return Container(
+      padding: EdgeInsets.all(20.0),
+      margin: EdgeInsets.all(20.0),
+      decoration: BoxDecoration(
+          border: Border.all(
+        color: Colors.grey,
+      )),
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(bottom: 8.0),
+            child: Image.network(image.url),
+          ),
+          Text(image.title)
+        ],
+      ),
     );
   }
 }
